fa9ca9897d1ee25b0949b59a18ce43a0e47c14c6
Bruno Felipe <bfaftrabalho@gmail.com>
2023-02-20 18:45:19 -0300

    Site Robotron completo e corrigido

--- Alura Cursos/0 - Front-End/JavaScript/JavaScript para Front-end/JavaScript manipulando o DOM/eu/-robotron-2000-projeto_inicial/js/main.js
+++ Alura Cursos/0 - Front-End/JavaScript/JavaScript para Front-end/JavaScript manipulando o DOM/eu/-robotron-2000-projeto_inicial/js/main.js
@@ -4,12 +4,23 @@ const estatistica = document.querySelectorAll("[data-estatistica]"); //Habilidad
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode); //parentNode -> Elemento Pai
-        atualizaEstatistica(evento.target.dataset.peca); //dataset -> Mostra todos os 'data-' que tem no elemento selecionado
+        atualizaEstatistica(evento.target.dataset.controle, evento.target.dataset.peca); //dataset -> Mostra todos os 'data-' que tem no elemento selecionado. Retorna o elemento
     })
 })
 
 function  manipulaDados (valorControle, elementoPai){ // Função para adicionar pontos as partes do robo
     let valor = elementoPai.querySelector("[data-contador]");
 
-    valorControle === "-" ? parseInt(valor.value) - 1 : parseInt(valor.value) + 1;
+    valorControle === "-" ? valor.value = parseInt(valor.value) - 1 : valor.value = parseInt(valor.value) + 1;
+}
+
+function atualizaEstatistica(valorControle, parteDoRobo){ //Função para atualizar os valores das estatiscas
+    if(valorControle === "-")
+        estatistica.forEach ((elemento) => {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[parteDoRobo][elemento.dataset.estatistica];
+        });
+    else
+        estatistica.forEach ((elemento) => {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[parteDoRobo][elemento.dataset.estatistica];
+        });
 }
