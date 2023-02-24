367945fd754a88e57ac7a5a2bf396c7cac8a104c
Bruno Felipe <bfaftrabalho@gmail.com>
2023-02-24 17:28:32 -0300

    03 inserindo dados no localStorage

--- Alura Cursos/0 - Front-End/Formação JavaScript/03 - JavaScript na Web armazendando dados no navegador/Aluno/js_localStorage-aula_1/js/main.js
+++ Alura Cursos/0 - Front-End/Formação JavaScript/03 - JavaScript na Web armazendando dados no navegador/Aluno/js_localStorage-aula_1/js/main.js
@@ -4,8 +4,14 @@ const lista = document.getElementById("lista");   // Pegamos a lista de itens qu
 form.addEventListener("submit", (evento) => {    // Monitoramos o envio do formulario
     evento.preventDefault();                      // Fazemos com que o evento padrão de envio do formulario seja cancelado
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    const nome = evento.target.elements['nome'];                // Elemento 'nome' dentro do form
+    const quantidade = evento.target.elements['quantidade'];    // Elemento 'quantidade' dentro do form
+
+    criaElemento(nome.value, quantidade.value);
     // Aqui coletamos os valores das tags 'nome' e 'quantidade' dentro do formulario
+
+    nome.value = "";        // Resetando o valor de nome para receber um novo ou o mesmo
+    quantidade.value = "";  // Resetando o valor da quantidade para receber um novo ou o mesmo
 })
 
 function criaElemento(nome, quantidade) {           // Função para alimentar a lista
@@ -19,4 +25,7 @@ function criaElemento(nome, quantidade) {           // Função para alimentar a
     novoItem.innerHTML += nome;         // Concatenando a tag <strong> com o valor 'nome'
 
     lista.appendChild(novoItem);    // Adicionando um novo item a lista.
+
+    localStorage.setItem("nome", nome);             // Dessa forma inserimos um objeto de nome: 'nome', e dado: 'nome'
+    localStorage.setItem("quantidade", quantidade); // O mesmo vale aqui para quantidade
 }
