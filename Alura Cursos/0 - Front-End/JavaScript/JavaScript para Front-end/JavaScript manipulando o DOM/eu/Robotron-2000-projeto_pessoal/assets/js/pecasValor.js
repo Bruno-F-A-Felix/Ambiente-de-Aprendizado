9df9f3b5af0bcd9d8078b5a69b696f5290e3ef5b
Bruno Felipe <bfaftrabalho@gmail.com>
2023-02-24 15:18:07 -0300

    Fazendo atualizações

--- Alura Cursos/0 - Front-End/JavaScript/JavaScript para Front-end/JavaScript manipulando o DOM/eu/Robotron-2000-projeto_pessoal/assets/js/pecasValor.js
+++ Alura Cursos/0 - Front-End/JavaScript/JavaScript para Front-end/JavaScript manipulando o DOM/eu/Robotron-2000-projeto_pessoal/assets/js/pecasValor.js
@@ -1,5 +1,6 @@
 const pecas = {
     "bracos": {
+        "vida": 10,
         "forca": 29,
         "poder": 35,
         "energia": -21,
@@ -7,27 +8,31 @@ const pecas = {
     },
 
     "blindagem": {
-        "forca": 41,
+        "vida": 50,
+        "forca": 31,
         "poder": 20,
-        "energia": 0,
+        "energia": 10,
         "velocidade": -20
     },
     "nucleos":{
-        "forca": 0,
-        "poder": 7,
+        "vida": 15,
+        "forca": -5,
+        "poder": 17,
         "energia": 48,
-        "velocidade": -24
+        "velocidade": -4
     },
     "pernas":{
+        "vida": 15,
         "forca": 27,
         "poder": 21,
         "energia": -32,
         "velocidade": 42
     },
     "foguetes":{
-        "forca": 0,
+        "vida": 10,
+        "forca": 6,
         "poder": 28,
         "energia": 0,
-        "velocidade": -2
+        "velocidade": 20
     }
 }
