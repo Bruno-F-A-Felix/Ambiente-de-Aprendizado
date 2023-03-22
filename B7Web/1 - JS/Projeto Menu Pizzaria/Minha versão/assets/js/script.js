5766dcf94ce5635af2bfb010b9fd8324c7301b71
Bruno Felipe <bfaftrabalho@gmail.com>
2023-03-22 15:17:12 -0300

    Falta resolver o bug do click

--- B7Web/1 - JS/Projeto Menu Pizzaria/Minha versão/assets/js/script.js
+++ B7Web/1 - JS/Projeto Menu Pizzaria/Minha versão/assets/js/script.js
@@ -1,14 +1,30 @@
 const qs = (el) => document.querySelector(el);
 const qsAll = (el) => document.querySelectorAll(el); // 'el' é uma vatiavel criada dentro do parametro da função. Seu valor em enviado quando chamado
 
-pizzaJson.forEach(element => {
+pizzaJson.forEach((element, index) => {
     let pizzaOption = qs('.pizza-item').cloneNode(true);  // Primeiro precisamos clonar um elemento molde para criarmos um novo e adiciona-lo em seguida
 
+    pizzaOption.querySelector('a').setAttribute('data-key', index);
     pizzaOption.querySelector('.pizza-item--img img').src = element.img;
     pizzaOption.querySelector('.pizza-item--price').innerHTML = `R$ ${element.price.toFixed(2)}`;
     pizzaOption.querySelector('.pizza-item--name').innerHTML = element.name;
     pizzaOption.querySelector('.pizza-item--desc').innerHTML = element.description;
 
     qs('.pizza-area').appendChild(pizzaOption);  // Dessa forma fazemos a adição de novos elementos, diferente do innerHTML que sobrescreve
+
+    pizzaOption.querySelector('a').addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        let key = e.parentNode;
+        console.log(key);
+
+        // qs('.pizzaBig img').src = pizzaJson[key].img;
+
+        qs('.pizzaWindowArea').style.opacity = 0;
+        qs('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(()=>{
+            qs('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+    });
 });
 
