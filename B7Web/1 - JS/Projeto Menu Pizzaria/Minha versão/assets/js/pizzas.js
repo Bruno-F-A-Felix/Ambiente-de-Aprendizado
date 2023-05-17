e6a2e8b6bf9973499edf9be29642142f10e154d0
Bruno Felipe <bfaftrabalho@gmail.com>
2023-05-17 18:48:41 -0300

    um novo recomeço

--- B7Web/1 - JS/Projeto Menu Pizzaria/Minha versão/assets/js/pizzas.js
+++ B7Web/1 - JS/Projeto Menu Pizzaria/Minha versão/assets/js/pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'assets/images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'assets/images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'assets/images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'assets/images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'assets/images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:6, name:'Moda da Casa', img:'assets/images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'assets/images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:1, name:'Mussarela', img:'assets/images/pizza.png', price:[20.19, 25.19, 30.19], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'assets/images/pizza2.png', price:[18.00, 23.00, 28.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'assets/images/pizza3.png', price:[17.45, 22.45, 27.45], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'assets/images/pizza4.png', price:[19.77, 24.77, 29.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'assets/images/pizza5.png', price:[21.43, 26.43, 31.43], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'assets/images/pizza6.png', price:[18.55, 23.55, 28.55], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'assets/images/pizza7.png', price:[22.36, 27.36, 32.36], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
---
+++ B7Web/1 - JS/Projeto Menu Pizzaria/Minha versão/assets/js/script.js
@@ -0,0 +1,4 @@
+// Variaveis para enxugar o código
+const qs = (el)=>document.querySelector(el);
+const qsAll = (el)=>document.querySelectorAll(el);
+
