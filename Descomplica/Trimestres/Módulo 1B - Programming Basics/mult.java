ab5c9f4ec07ef62c89d5e86b960030e49b5dbddf
Bruno Felix <bfaftrabalho@gmail.com>
2024-05-31 20:16:06 -0300

    Criando alguns programas em java

---
+++ Descomplica/Trimestres/Módulo 1B - Programming Basics/mult.java
@@ -0,0 +1,17 @@
+public class mult {
+    public static void main(String entrada[]) {
+        //Declaração de variaveis
+        int n1, n2, soma;
+
+        //Entrada de dados
+        n1 = Integer.parseInt(entrada[0]);
+        n2 = Integer.parseInt(entrada[1]);
+
+        //Processamento
+        soma = n1 + n2;
+        
+        //Saída dos resultados
+        System.out.println(n1 + " + " + n2 + " = " + soma);
+        System.exit(0);
+    }
+}
---
+++ Descomplica/Trimestres/Módulo 1B - Programming Basics/program.java
@@ -0,0 +1,17 @@
+//nome da classe
+class program{
+    public static void main(String[] args) {
+        //declaração de variáveis
+        int inteiro = 47;
+        char caracter = 'F';
+        double real = 1.65;
+        String frase = "Lucy Mari ";
+        boolean VF=true;
+
+        if (VF == true){
+            System.out.println("Eu sou a " + frase + "tenho " + inteiro + "anos e tenho " + real + "m de altura");
+        }
+
+        System.exit(0);
+    }
+}
